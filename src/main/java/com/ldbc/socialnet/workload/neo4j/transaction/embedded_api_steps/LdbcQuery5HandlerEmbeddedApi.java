75326583c6ba36c524e7463d88b180498eab5ff9
Alex Averbuch <alex.averbuch@gmail.com>
2014-02-10 15:37:20 +0100

    update ldbc_driver modify two query implementations to use Guava ImmutableList class instead of removed iteratorToList() method from ldbc_driver modify two query implementations to use stackTraceToString from ConcurrentErrorReporter (in ldbc_driver) instead of old Utils class

--- src/main/java/com/ldbc/socialnet/workload/neo4j/transaction/embedded_api_steps/LdbcQuery5HandlerEmbeddedApi.java
+++ src/main/java/com/ldbc/socialnet/workload/neo4j/transaction/embedded_api_steps/LdbcQuery5HandlerEmbeddedApi.java
@@ -1,48 +1,43 @@
 package com.ldbc.socialnet.workload.neo4j.transaction.embedded_api_steps;
 
-import java.util.List;
-
-import org.apache.log4j.Logger;
-import org.neo4j.cypher.javacompat.ExecutionEngine;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Transaction;
-
+import com.google.common.collect.ImmutableList;
 import com.ldbc.driver.DbException;
 import com.ldbc.driver.OperationHandler;
 import com.ldbc.driver.OperationResult;
+import com.ldbc.driver.runner.ConcurrentErrorReporter;
 import com.ldbc.driver.workloads.ldbc.socnet.interactive.LdbcQuery5;
 import com.ldbc.driver.workloads.ldbc.socnet.interactive.LdbcQuery5Result;
 import com.ldbc.socialnet.workload.neo4j.Neo4jConnectionStateEmbedded;
 import com.ldbc.socialnet.workload.neo4j.transaction.LdbcTraversers;
 import com.ldbc.socialnet.workload.neo4j.transaction.Neo4jQuery5;
-import com.ldbc.socialnet.workload.neo4j.utils.Utils;
+import org.apache.log4j.Logger;
+import org.neo4j.cypher.javacompat.ExecutionEngine;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Transaction;
 
-public class LdbcQuery5HandlerEmbeddedApi extends OperationHandler<LdbcQuery5>
-{
-    private final static Logger logger = Logger.getLogger( LdbcQuery5HandlerEmbeddedApi.class );
+import java.util.List;
+
+public class LdbcQuery5HandlerEmbeddedApi extends OperationHandler<LdbcQuery5> {
+    private final static Logger logger = Logger.getLogger(LdbcQuery5HandlerEmbeddedApi.class);
 
     @Override
-    protected OperationResult executeOperation( LdbcQuery5 operation ) throws DbException
-    {
-        ExecutionEngine engine = ( (Neo4jConnectionStateEmbedded) dbConnectionState() ).executionEngine();
-        GraphDatabaseService db = ( (Neo4jConnectionStateEmbedded) dbConnectionState() ).db();
-        LdbcTraversers traversers = ( (Neo4jConnectionStateEmbedded) dbConnectionState() ).traversers();
-        Neo4jQuery5 query5 = new Neo4jQuery5EmbeddedApi( traversers );
+    protected OperationResult executeOperation(LdbcQuery5 operation) throws DbException {
+        ExecutionEngine engine = ((Neo4jConnectionStateEmbedded) dbConnectionState()).executionEngine();
+        GraphDatabaseService db = ((Neo4jConnectionStateEmbedded) dbConnectionState()).db();
+        LdbcTraversers traversers = ((Neo4jConnectionStateEmbedded) dbConnectionState()).traversers();
+        Neo4jQuery5 query5 = new Neo4jQuery5EmbeddedApi(traversers);
         List<LdbcQuery5Result> result = null;
 
         // TODO find way to do this
         int resultCode = 0;
-        try (Transaction tx = db.beginTx())
-        {
-            result = Utils.iteratorToList( query5.execute( db, engine, operation ) );
+        try (Transaction tx = db.beginTx()) {
+            result = ImmutableList.copyOf((query5.execute(db, engine, operation));
             tx.success();
-        }
-        catch ( Exception e )
-        {
-            logger.error(String.format("Error executing query\n%s\n%s", operation.toString(), Utils.stackTraceToString(e)));
+        } catch (Exception e) {
+            logger.error(String.format("Error executing query\n%s\n%s", operation.toString(), ConcurrentErrorReporter.stackTraceToString(e)));
             resultCode = -1;
         }
 
-        return operation.buildResult( resultCode, result );
+        return operation.buildResult(resultCode, result);
     }
 }
--- src/main/java/com/ldbc/socialnet/workload/neo4j/utils/Utils.java
+++ src/main/java/com/ldbc/socialnet/workload/neo4j/utils/Utils.java
@@ -1,43 +1,14 @@
 package com.ldbc.socialnet.workload.neo4j.utils;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-public class Utils
-{
-    public static String[] copyArrayAndAddElement( String[] oldArray, String newElement )
-    {
-        if ( null == oldArray )
-        {
-            return new String[] { newElement };
-        }
-        else
-        {
+public class Utils {
+    public static String[] copyArrayAndAddElement(String[] oldArray, String newElement) {
+        if (null == oldArray) {
+            return new String[]{newElement};
+        } else {
             String[] newArray = new String[oldArray.length + 1];
-            System.arraycopy( oldArray, 0, newArray, 0, oldArray.length );
+            System.arraycopy(oldArray, 0, newArray, 0, oldArray.length);
             newArray[newArray.length - 1] = newElement;
             return newArray;
         }
-    }
-
-    public static String stackTraceToString( Exception e )
-    {
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter( sw );
-        e.printStackTrace( pw );
-        return sw.toString();
-    }
-
-    public static <T> List<T> iteratorToList( Iterator<T> iterator )
-    {
-        List<T> list = new ArrayList<T>();
-        while ( iterator.hasNext() )
-        {
-            list.add( iterator.next() );
-        }
-        return list;
     }
 }
