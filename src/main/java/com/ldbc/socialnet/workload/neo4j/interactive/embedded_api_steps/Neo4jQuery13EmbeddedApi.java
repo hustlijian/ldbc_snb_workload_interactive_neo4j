dc02a557518c42ba81ffd3ceeddf369ca97410fb
Alex Averbuch <alex.averbuch@gmail.com>
2014-09-06 19:35:43 +0200

    add query13 api/steps impl that is consistent with latest spec

--- src/main/java/com/ldbc/socialnet/workload/neo4j/interactive/embedded_api_steps/Neo4jQuery13EmbeddedApi.java
+++ src/main/java/com/ldbc/socialnet/workload/neo4j/interactive/embedded_api_steps/Neo4jQuery13EmbeddedApi.java
@@ -1,10 +1,15 @@
 package com.ldbc.socialnet.workload.neo4j.interactive.embedded_api_steps;
 
+import com.google.common.collect.Iterators;
+import com.google.common.collect.Lists;
 import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcQuery13;
 import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcQuery13Result;
+import com.ldbc.socialnet.workload.neo4j.Domain;
 import com.ldbc.socialnet.workload.neo4j.interactive.LdbcTraversers;
 import com.ldbc.socialnet.workload.neo4j.interactive.Neo4jQuery13;
-import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphalgo.GraphAlgoFactory;
+import org.neo4j.graphalgo.PathFinder;
+import org.neo4j.graphdb.*;
 
 import java.util.Iterator;
 
@@ -22,6 +27,18 @@ public class Neo4jQuery13EmbeddedApi extends Neo4jQuery13<GraphDatabaseService> 
 
     @Override
     public Iterator<LdbcQuery13Result> execute(GraphDatabaseService db, LdbcQuery13 operation) {
-        return null;
+        Iterator<Node> person1Iterator = db.findNodesByLabelAndProperty(Domain.Nodes.Person, Domain.Person.ID, operation.person1Id()).iterator();
+        if (false == person1Iterator.hasNext()) return Iterators.emptyIterator();
+        final Node person1 = person1Iterator.next();
+
+        Iterator<Node> person2Iterator = db.findNodesByLabelAndProperty(Domain.Nodes.Person, Domain.Person.ID, operation.person2Id()).iterator();
+        if (false == person2Iterator.hasNext()) return Iterators.emptyIterator();
+        final Node person2 = person2Iterator.next();
+
+        PathFinder<Path> finder = GraphAlgoFactory.shortestPath(PathExpanders.forTypeAndDirection(Domain.Rels.KNOWS, Direction.BOTH), Integer.MAX_VALUE);
+        Path shortestPath = finder.findSinglePath(person1, person2);
+
+        int shortestPathLength = (null == shortestPath) ? -1 : shortestPath.length();
+        return Lists.newArrayList(new LdbcQuery13Result(shortestPathLength)).iterator();
     }
 }
