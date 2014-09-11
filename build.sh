d9e7e1823e54bf45fd47aec7c3c069172d08a8db
Alex Averbuch <alex.averbuch@gmail.com>
2014-09-11 10:32:40 +0200

    add neo4j-jdbc to build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,10 @@ STEPS="steps"
 STEPS_VER="0.2-SNAPSHOT"
 STEPS_JAR="steps/target/steps-"${STEPS_VER}".jar"
 
+JDBC="neo4j-jdbc"
+JDBC_VER="2.1.4-SNAPSHOT"
+JDBC_JAR="neo4j-jdbc/target/neo4j-jdbc-"${JDBC_VER}".jar"
+
 IN_PROJECT_MVN_REPO="lib"
 
 git submodule update --init
@@ -21,7 +25,12 @@ cd $STEPS
 mvn clean package -Dmaven.compiler.source=1.7 -Dmaven.compiler.target=1.7
 cd ..
 
+cd $JDBC
+mvn clean package -Dmaven.compiler.source=1.7 -Dmaven.compiler.target=1.7
+cd ..
+
 mvn install:install-file -DlocalRepositoryPath=$IN_PROJECT_MVN_REPO -DcreateChecksum=true -Dpackaging=jar -Dfile=$LDBC_CORE_JAR -DgroupId=com.ldbc.driver -DartifactId=core -Dversion=$LDBC_CORE_VER
 mvn install:install-file -DlocalRepositoryPath=$IN_PROJECT_MVN_REPO -DcreateChecksum=true -Dpackaging=jar -Dfile=$STEPS_JAR -DgroupId=org.neo4j.traversal -DartifactId=steps -Dversion=$STEPS_VER
+mvn install:install-file -DlocalRepositoryPath=$IN_PROJECT_MVN_REPO -DcreateChecksum=true -Dpackaging=jar -Dfile=$JDBC_JAR -DgroupId=org.neo4j.jdbc -DartifactId=neo4j-jdbc -Dversion=$JDBC_VER
 
 mvn clean compile -Dmaven.compiler.source=1.7 -Dmaven.compiler.target=1.7 -X
