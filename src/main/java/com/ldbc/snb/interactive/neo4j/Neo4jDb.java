1619fe7cb0394b168291d5f705f64bd0eab9d370
Alex Averbuch <alex.averbuch@gmail.com>
2014-09-26 12:40:05 +0200

    remote old dshini related comments

--- src/main/java/com/ldbc/snb/interactive/neo4j/Neo4jDb.java
+++ src/main/java/com/ldbc/snb/interactive/neo4j/Neo4jDb.java
@@ -8,17 +8,6 @@ import org.apache.log4j.Logger;
 import java.io.File;
 import java.util.Map;
 
-/**
- * Neo4j client for LDBC DShini workload
- * <p/>
- * Properties to set:
- * <p/>
- * neo4j.url=http://localhost:7474/db/data <br>
- * neo4j.clear=false <br>
- * neo4j.path=/tmp/db <br>
- * neo4j.dbtype=embedded <br>
- */
-
 public class Neo4jDb extends Db {
     private static Logger logger = Logger.getLogger(Neo4jDb.class);
 
