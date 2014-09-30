5ccf7ebf30a04edd8b840cca24deebfe751aa54c
Alex Averbuch <alex.averbuch@gmail.com>
2014-09-30 14:43:19 +0200

    replace import org.neo4j.kernel.impl.util.FileUtils with import org.apache.commons.io.FileUtils

--- data-import/src/test/java/com/ldbc/snb/interactive/neo4j/utils/TempIndexPerformanceTests.java
+++ data-import/src/test/java/com/ldbc/snb/interactive/neo4j/utils/TempIndexPerformanceTests.java
@@ -1,5 +1,10 @@
 package com.ldbc.snb.interactive.neo4j.utils;
 
+import com.ldbc.snb.interactive.neo4j.load.tempindex.*;
+import org.apache.commons.io.FileUtils;
+import org.junit.Ignore;
+import org.junit.Test;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -7,20 +12,8 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.Ignore;
-import org.junit.Test;
-import org.neo4j.kernel.impl.util.FileUtils;
-
-import com.ldbc.snb.interactive.neo4j.load.tempindex.DirectMemoryMapDbTempIndexFactory;
-import com.ldbc.snb.interactive.neo4j.load.tempindex.HashMapTempIndexFactory;
-import com.ldbc.snb.interactive.neo4j.load.tempindex.MemoryMapDbTempIndexFactory;
-import com.ldbc.snb.interactive.neo4j.load.tempindex.PersistentMapDbTempIndexFactory;
-import com.ldbc.snb.interactive.neo4j.load.tempindex.TempIndex;
-import com.ldbc.snb.interactive.neo4j.load.tempindex.TroveTempIndexFactory;
-
 @Ignore
-public class TempIndexPerformanceTests
-{
+public class TempIndexPerformanceTests {
     /*
     // -Xmx40g --> 421,000,000
     TempIndex<Long, Long> x = new TroveTempIndexFactory().create();
@@ -31,68 +24,62 @@ public class TempIndexPerformanceTests
     */
 
     @Test
-    public void shouldPerfTestTempIndexes() throws IOException
-    {
+    public void shouldPerfTestTempIndexes() throws IOException {
         // Given
         String indexPath = "testIndex1/";
-        FileUtils.deleteRecursively( new File( indexPath ) );
-        File indexDir = new File( indexPath );
+        FileUtils.deleteDirectory(new File(indexPath));
+        File indexDir = new File(indexPath);
         indexDir.mkdir();
 
         // When
         List<TempIndex<Long, Long>> indexes = new ArrayList<TempIndex<Long, Long>>();
-        indexes.add( new PersistentMapDbTempIndexFactory( indexDir ).create() );
-        indexes.add( new MemoryMapDbTempIndexFactory().create() );
-        indexes.add( new DirectMemoryMapDbTempIndexFactory().create() );
-        indexes.add( new TroveTempIndexFactory().create() );
-        indexes.add( new HashMapTempIndexFactory().create() );
+        indexes.add(new PersistentMapDbTempIndexFactory(indexDir).create());
+        indexes.add(new MemoryMapDbTempIndexFactory().create());
+        indexes.add(new DirectMemoryMapDbTempIndexFactory().create());
+        indexes.add(new TroveTempIndexFactory().create());
+        indexes.add(new HashMapTempIndexFactory().create());
 
         // Then
-        for ( TempIndex<Long, Long> index : indexes )
-        {
-            doWritePerfTest( index );
-            doReadPerfTest( index );
+        for (TempIndex<Long, Long> index : indexes) {
+            doWritePerfTest(index);
+            doReadPerfTest(index);
             index.shutdown();
             System.out.println();
         }
 
-        FileUtils.deleteRecursively( new File( indexPath ) );
+        FileUtils.deleteDirectory(new File(indexPath));
     }
 
-    private void doWritePerfTest( TempIndex<Long, Long> index )
-    {
-        Random random = new Random( 42l );
+    private void doWritePerfTest(TempIndex<Long, Long> index) {
+        Random random = new Random(42l);
         long startTime = System.currentTimeMillis();
-        for ( long counter = 0; counter < 10000000; counter++ )
-        {
-            index.put( counter, random.nextLong() );
+        for (long counter = 0; counter < 10000000; counter++) {
+            index.put(counter, random.nextLong());
         }
         long runtime = System.currentTimeMillis() - startTime;
-        System.out.println( "Write["
-                            + index.getClass().getSimpleName()
-                            + "]: "
-                            + String.format(
-                                    "Time: %d min, %d sec",
-                                    TimeUnit.MILLISECONDS.toMinutes( runtime ),
-                                    TimeUnit.MILLISECONDS.toSeconds( runtime )
-                                            - TimeUnit.MINUTES.toSeconds( TimeUnit.MILLISECONDS.toMinutes( runtime ) ) ) );
+        System.out.println("Write["
+                + index.getClass().getSimpleName()
+                + "]: "
+                + String.format(
+                "Time: %d min, %d sec",
+                TimeUnit.MILLISECONDS.toMinutes(runtime),
+                TimeUnit.MILLISECONDS.toSeconds(runtime)
+                        - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(runtime))));
     }
 
-    private void doReadPerfTest( TempIndex<Long, Long> index )
-    {
+    private void doReadPerfTest(TempIndex<Long, Long> index) {
         long startTime = System.currentTimeMillis();
-        for ( long counter = 0; counter < 10000000; counter++ )
-        {
-            index.get( counter );
+        for (long counter = 0; counter < 10000000; counter++) {
+            index.get(counter);
         }
         long runtime = System.currentTimeMillis() - startTime;
-        System.out.println( "Read["
-                            + index.getClass().getSimpleName()
-                            + "]: "
-                            + String.format(
-                                    "Time: %d min, %d sec",
-                                    TimeUnit.MILLISECONDS.toMinutes( runtime ),
-                                    TimeUnit.MILLISECONDS.toSeconds( runtime )
-                                            - TimeUnit.MINUTES.toSeconds( TimeUnit.MILLISECONDS.toMinutes( runtime ) ) ) );
+        System.out.println("Read["
+                + index.getClass().getSimpleName()
+                + "]: "
+                + String.format(
+                "Time: %d min, %d sec",
+                TimeUnit.MILLISECONDS.toMinutes(runtime),
+                TimeUnit.MILLISECONDS.toSeconds(runtime)
+                        - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(runtime))));
     }
 }
