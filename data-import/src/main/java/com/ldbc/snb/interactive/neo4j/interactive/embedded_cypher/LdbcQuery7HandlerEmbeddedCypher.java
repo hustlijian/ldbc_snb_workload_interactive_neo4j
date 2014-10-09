947158c6c25766eec510676ef308e6f8171cf470
Alex Averbuch <alex.averbuch@gmail.com>
2014-10-09 13:41:26 +0200

    (temporary) add printouts to queries, to help with tracking which queries are not finishing

--- data-import/src/main/java/com/ldbc/snb/interactive/neo4j/interactive/embedded_cypher/LdbcQuery7HandlerEmbeddedCypher.java
+++ data-import/src/main/java/com/ldbc/snb/interactive/neo4j/interactive/embedded_cypher/LdbcQuery7HandlerEmbeddedCypher.java
@@ -17,6 +17,8 @@ import java.util.List;
 public class LdbcQuery7HandlerEmbeddedCypher extends OperationHandler<LdbcQuery7> {
     @Override
     protected OperationResultReport executeOperation(LdbcQuery7 operation) throws DbException {
+        // TODO remove
+        System.out.println("STARTED: " + operation.toString());
         ExecutionEngine engine = ((Neo4jConnectionState) dbConnectionState()).executionEngine();
         GraphDatabaseService db = ((Neo4jConnectionState) dbConnectionState()).db();
         List<LdbcQuery7Result> result;
@@ -31,7 +33,8 @@ public class LdbcQuery7HandlerEmbeddedCypher extends OperationHandler<LdbcQuery7
                     ConcurrentErrorReporter.stackTraceToString(e));
             throw new DbException(errMsg, e);
         }
-
+        // TODO remove
+        System.out.println("FINISHED: " + operation.toString());
         return operation.buildResult(resultCode, result);
     }
 }
